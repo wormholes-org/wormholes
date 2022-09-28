bff2b60a26ff94761120fdaee95cb931812e90c8
MiNix <57319138@qq.com>
2022-09-28 19:42:05 +0800

    fix: remove useless code

--- consensus/istanbul/ibft/core/preprepare.go
+++ consensus/istanbul/ibft/core/preprepare.go
@@ -130,51 +130,6 @@ func (c *core) handlePreprepare(msg *ibfttypes.Message, src istanbul.Validator) 
 }
 
 func (c *core) handlePreprepareStep1(msg *ibfttypes.Message, src istanbul.Validator) error {
-	//logger := c.logger.New("from", src, "state", c.state)
-
-	// Decode PRE-PREPARE
-	var preprepare *istanbul.Preprepare
-	err := msg.Decode(&preprepare)
-	if err != nil {
-		return istanbulcommon.ErrFailedDecodePreprepare
-	}
-
-	log.Info("ibftConsensus: handlePreprepare",
-		"no", preprepare.Proposal.Number().Uint64(),
-		"round", preprepare.View.Round.String(),
-		"from", src.Address().Hex(),
-		"self", c.address.Hex(),
-		"hash", preprepare.Proposal.Hash().Hex(),
-		"state", c.state)
-	// Ensure we have the same view with the PRE-PREPARE message
-	// If it is old message, see if we need to broadcast COMMIT
-	if err := c.checkMessage(ibfttypes.MsgPreprepare, preprepare.View); err != nil {
-		log.Error("ibftConsensus: handlePreprepare checkMessage",
-			"no", preprepare.Proposal.Number().Uint64(),
-			"round", preprepare.View.Round.String(),
-			"err", err.Error(),
-			"hash", preprepare.Proposal.Hash().Hex(),
-			"self", c.address.Hex())
-		if err == istanbulcommon.ErrOldMessage {
-			// Get validator set for the given proposal
-			valSet := c.backend.ParentValidators(preprepare.Proposal).Copy()
-			previousProposer := c.backend.GetProposer(preprepare.Proposal.Number().Uint64() - 1)
-			valSet.CalcProposer(previousProposer, preprepare.View.Round.Uint64())
-			// Broadcast COMMIT if it is an existing block
-			// 1. The proposer needs to be a proposer matches the given (Sequence + Round)
-			// 2. The given block must exist
-			if valSet.IsProposer(src.Address()) && c.backend.HasPropsal(preprepare.Proposal.Hash(), preprepare.Proposal.Number()) {
-				log.Info("ibftConsensus: handlePreprepare sendCommitForOldBlock",
-					"no", preprepare.Proposal.Number().String(),
-					"round", preprepare.View.Round.String(),
-					"hash", preprepare.Proposal.Hash().Hex(),
-					"self", c.address.Hex())
-				c.sendCommitForOldBlock(preprepare.View, preprepare.Proposal.Hash())
-				return nil
-			}
-		}
-		return err
-	}
 
 	if c.IsProposer() { //is proposer
 		//TODO: collect random seed
@@ -185,37 +140,6 @@ func (c *core) handlePreprepareStep1(msg *ibfttypes.Message, src istanbul.Valida
 			//TODO: Ready To PreprepareStep2
 			c.AssambleNewBlockWithRandomData()
 		}
-
-		//// If it is locked, it can only process on the locked block.
-		//// Passing verifyPrepare and checkMessage implies it is processing on the locked block since it was verified in the Preprepared state.
-		//if err := c.verifyPrepare(prepare, src); err != nil {
-		//	log.Info("ibftConsensus: handlePrepare verifyPrepare",
-		//		"no", prepare.View.Sequence,
-		//		"round", prepare.View.Round.String(),
-		//		"hash", prepare.Digest.Hex(),
-		//		"self", c.address.Hex(),
-		//		"err", err)
-		//	return err
-		//}
-		//
-		//c.acceptPrepare(msg, src)
-		//// Change to Prepared state if we've received enough PREPARE messages or it is locked
-		//// and we are in earlier state before Prepared state.
-		//if ((c.current.IsHashLocked() && prepare.Digest == c.current.GetLockedHash()) || c.current.GetPrepareOrCommitSize() >= c.QuorumSize()) &&
-		//	c.state.Cmp(ibfttypes.StatePrepared) < 0 {
-		//	c.current.LockHash()
-		//	c.setState(ibfttypes.StatePrepared)
-		//	log.Info("ibftConsensus: handlePrepare sendCommit",
-		//		"no", prepare.View.Sequence,
-		//		"round", prepare.View.Round,
-		//		"prepare+commitSize", c.current.GetPrepareOrCommitSize(),
-		//		"hash", prepare.Digest.Hex(),
-		//		"self", c.address.Hex(),
-		//	)
-		//	c.sendCommit()
-		//}
-		//
-		//return nil
 	} else {
 		if !c.valSet.IsProposer(src.Address()) {
 			//TODO: start send random seed
@@ -230,7 +154,7 @@ func (c *core) AssambleNewBlockWithRandomData() {
 	var blk = c.current.Preprepare.Proposal
 	var randSeedData = randSeedMessages.Encode()
 	blk.SetExtra(randSeedData)
-	log.Info(string(randSeedData))
+	log.Info("random data msg: " + string(randSeedData) + "[csss]")
 	//打印随机数
 	//分配奖励
 	//TODO: Start preprepare round
