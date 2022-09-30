5fd1025ff7efe113b0a896199a93132ce5594987
Sai <51160269@qq.com>
2022-09-30 12:23:37 +0800

    perf: File content adjustment

--- wormholes_install.sh
+++ wormholes_install.sh
@@ -20,34 +20,23 @@ fi
 docker stop wormholes > /dev/null 2>&1
 docker rm wormholes > /dev/null 2>&1
 docker rmi wormholestech/wormholes:v1 > /dev/null 2>&1
-#docker rmi wormholes:v1 > /dev/null 2>&1
-#while :
-#do
-    #if [ -z "$ky" ];then
-        #echo "密钥不能为空"
-        #continue
-	#fi
-	#break
-#done
 if [ -d /wm/.wormholes/keystore ]; then
-    read -p "是否要清空wormholes区块链历史数据,清空请按y,不清空请直接回车：" xyz
+    read -p "If you want to clear the historical data of the wormholes blockchain, please press y to clear it, if not, please press Enter directly:" xyz
     if [ "$xyz" = 'y' ];then
         rm -rf /wm/.wormholes
-	read -p "请输入您的私钥：" ky
+	read -p "Please enter your private key:" ky
     else
-        echo "不清空"
+        echo "not empty"
     fi
 fi
-#mkdir -p /wm
 mkdir -p /wm/.wormholes/wormholes
 if [ -n "$ky" ]; then
     echo $ky > /wm/.wormholes/wormholes/nodekey
 fi
-#docker run -id -e KEY=$ky -p 30303:30303 -p 8545:8545 -v /var/lib/wormholes:/root/.wormholes --name wormholes wormholestech/wormholes:v1
 docker run -id -e KEY=$ky  -p 30303:30303 -p 8545:8545 -v /wm/.wormholes:/wm/.wormholes --name wormholes wormholestech/wormholes:v1
 
 
-echo "您的私钥是:" 
+echo "Your private key is:"
 sleep 6
 docker exec -it wormholes /usr/bin/cat /wm/.wormholes/wormholes/nodekey
 #cat /wm/.wormholes/geth/nodekey
