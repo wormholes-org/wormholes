183c1b4b407309423885f4cfa4ce4465b15e63a4
mindcarver <626984947@qq.com>
2022-10-20 09:08:17 +0800

    fix:worker unit test

--- miner/worker_test.go
+++ miner/worker_test.go
@@ -476,7 +476,7 @@ func testAdjustInterval(t *testing.T, chainConfig *params.ChainConfig, engine co
 			estimate = estimate*(1-intervalAdjustRatio) + intervalAdjustRatio*(min-intervalAdjustBias)
 			wantMinInterval, wantRecommitInterval = 3*time.Second, time.Duration(estimate)*time.Nanosecond
 		case 3:
-			wantMinInterval, wantRecommitInterval = 3*time.Second, 3*time.Second
+			wantMinInterval, wantRecommitInterval = 2*time.Second, 2*time.Second
 		}
 
 		// Check interval
