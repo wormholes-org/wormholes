ba1aadcf31944813c4ceefe3e98324932acbcd00
Sai <51160269@qq.com>
2022-11-15 11:26:59 +0800

    perf: change prevHeight to worker

--- miner/worker.go
+++ miner/worker.go
@@ -214,6 +214,9 @@ type worker struct {
 	emptyHandleFlag bool
 	cacheHeight     *big.Int
 	emptyTimer      *time.Timer
+
+	//seal data
+	prevHeight uint64
 }
 
 func newWorker(handler Handler, config *Config, chainConfig *params.ChainConfig, engine consensus.Engine, eth Backend, mux *event.TypeMux, isLocalBlock func(*types.Block) bool, init bool) *worker {
@@ -248,6 +251,7 @@ func newWorker(handler Handler, config *Config, chainConfig *params.ChainConfig,
 		notifyBlockCh:      make(chan *types.OnlineValidatorList, 1),
 		emptyTimestamp:     time.Now().Unix(),
 		emptyHandleFlag:    false,
+		prevHeight:         eth.BlockChain().CurrentBlock().NumberU64(),
 	}
 
 	if _, ok := engine.(consensus.Istanbul); ok || !chainConfig.IsQuorum || chainConfig.Clique != nil {
@@ -749,11 +753,9 @@ func (w *worker) mainLoop() {
 // push them to consensus engine.
 func (w *worker) taskLoop() {
 	var (
-		stopCh     chan struct{}
-		prev       common.Hash
-		prevHeight uint64
+		stopCh chan struct{}
+		prev   common.Hash
 	)
-	prevHeight = 0
 	// interrupt aborts the in-flight sealing task.
 	interrupt := func() {
 		if stopCh != nil {
@@ -779,12 +781,12 @@ func (w *worker) taskLoop() {
 			if sealHash == prev {
 				continue
 			}
-			if prevHeight >= task.block.NumberU64() {
+			if w.prevHeight >= task.block.NumberU64() {
 				continue
 			}
 			// Interrupt previous sealing operation
 			interrupt()
-			stopCh, prev, prevHeight = make(chan struct{}), sealHash, task.block.NumberU64()
+			stopCh, prev, w.prevHeight = make(chan struct{}), sealHash, task.block.NumberU64()
 
 			if w.skipSealHook != nil && w.skipSealHook(task) {
 				continue
@@ -1520,6 +1522,9 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 		GasLimit:   core.CalcGasLimit(parent.GasLimit(), w.config.GasCeil),
 		Extra:      w.extra,
 		Time:       uint64(timestamp),
+	}
+	if header.Number.Uint64() <= w.prevHeight {
+		return
 	}
 	// Set baseFee and GasLimit if we are on an EIP-1559 chain
 	if w.chainConfig.IsLondon(header.Number) {
