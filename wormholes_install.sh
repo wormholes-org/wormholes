4e63690f1a4e734150ff0ac165038c247c5bb15b
Dzeqkon <2321688645@qq.com>
2022-12-12 13:05:00 +0800

    perf:wormholes install script

--- wormholes_install.sh
+++ wormholes_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #check docker cmd
+echo "Script version Number: v0.10.3"
 which docker >/dev/null 2>&1
 if  [ $? -ne 0 ] ; then
         echo "docker not found, please install first!"
@@ -17,39 +18,84 @@ if [ $? -ne 0 ] ; then
         exit
 fi
 
-docker stop wormholes > /dev/null 2>&1
-docker rm wormholes > /dev/null 2>&1
-docker rmi wormholestech/wormholes:v1 > /dev/null 2>&1
-if [ -d /wm/.wormholes/wormholes ]; then
-    read -p "If you want to clear the historical data of the wormholes blockchain, please press y to clear it, if not, please press Enter directly:" xyz
-    if [ "$xyz" = 'y' ];then
-        cp /wm/.wormholes/wormholes/nodekey /wm/nodekey
-        rm -rf /wm/.wormholes
-        mkdir -p /wm/.wormholes/wormholes
-        mv /wm/nodekey /wm/.wormholes/wormholes/nodekey
-    else
-        echo "not empty"
-    fi
+vt5=1670383155
+vl=$(wget https://docker.wormholes.com/version>/dev/null 2>&1 && cat version|awk '{print $1}')
+vr=$(cat version|awk '{print $2}' && rm version)
+worm=$(docker images|grep "wormholestech/wormholes"|grep "v1")
+if [ -n "$worm" ];then
+        ct=$(docker inspect wormholestech/wormholes:v1 -f {{.Created}})
+        cts=$(date -d "$ct" +%s)
+        if [ $cts -eq $vl ];then
+                container=$(docker ps -a|awk '{if($NF == "wormholes")print $0}')
+                if [[ $container =~ "Up" ]];then
+                        while true
+                        do
+                                key=$(docker exec -it wormholes /usr/bin/ls -l /wm/.wormholes/wormholes/nodekey)
+                                if [ -n "$key" ];then
+                                        echo -e "It is the latest version: $vr \nYour private key:"
+                                        docker exec -it wormholes /usr/bin/cat .wormholes/wormholes/nodekey
+                                        echo -e "\n"
+                                        exit 0
+                                else
+                                        sleep 5s
+                                fi
+                        done
+                elif [[ $container =~ "Exited" ]];then
+                        echo -e "Your peer isn't running\nYou can use 'docker start wormholes' to start the node"
+                        exit 0
+                else
+                        docker rm wormholes
+                        read -p "Enter your private key：" ky
+                fi
+        elif [ $cts -lt $vl ];then
+                docker stop wormholes > /dev/null 2>&1
+                docker rm wormholes > /dev/null 2>&1
+                docker rmi wormholestech/wormholes:v1 > /dev/null 2>&1
+                if [ $cts -lt $vt5 ];then
+                        if [ -f /wm/.wormholes/wormholes/nodekey ];then
+                                echo "Clearing historical data ............"
+                                cp /wm/.wormholes/wormholes/nodekey /wm/nodekey
+                                rm -rf /wm/.wormholes
+                                mkdir -p /wm/.wormholes/wormholes
+                                mv /wm/nodekey /wm/.wormholes/wormholes/
+                        else
+                                read -p "Enter your private key：" ky
+                        fi
+                elif [ $cts -ge $vt5 ];then
+                        if [ ! -f /wm/.wormholes/wormholes/nodekey ];then
+                                read -p "Enter your private key：" ky
+                        fi
+                fi
+        fi
 else
-    read -p "Please enter your private key:" ky
+        read -p "Enter your private key：" ky
 fi
 
 if [ -n "$ky" ]; then
-    if [ ${#ky} -eq 64 ];then
         mkdir -p /wm/.wormholes/wormholes
-        echo $ky > /wm/.wormholes/wormholes/nodekey
-    elif [ ${#ky} -eq 66 ] && ([ ${ky:0:2} == "0x" ] || [ ${ky:0:2} == "0X" ]);then
-        mkdir -p /wm/.wormholes/wormholes
-        echo ${ky:2:64} > /wm/.wormholes/wormholes/nodekey
-    else
-        echo "the nodekey format is not correct"
-        exit -1
-    fi
+        if [ ${#ky} -eq 64 ];then
+                echo $ky > /wm/.wormholes/wormholes/nodekey
+        elif [ ${#ky} -eq 66 ] && ([ ${ky:0:2} == "0x" ] || [ ${ky:0:2} == "0X" ]);then
+                echo ${ky:2:64} > /wm/.wormholes/wormholes/nodekey
+        else
+                echo "the nodekey format is not correct"
+                exit 1
+        fi
 fi
-docker run -id -e KEY=$ky  -p 30303:30303 -p 8545:8545 -v /wm/.wormholes:/wm/.wormholes --name wormholes wormholestech/wormholes:v1
 
+docker run -id -p 30303:30303 -p 8545:8545 -v /wm/.wormholes:/wm/.wormholes --name wormholes wormholestech/wormholes:v1 >/dev/null 2>&1 &
 
-echo "Your private key is:"
-sleep 6
-docker exec -it wormholes /usr/bin/cat /wm/.wormholes/wormholes/nodekey
-#cat /wm/.wormholes/geth/nodekey
+while true
+do
+        s=$(docker ps -a|grep "Up"|awk '{if($NF == "wormholes") print $NF}'|wc -l)
+        key=$(docker exec -it wormholes /usr/bin/ls -l /wm/.wormholes/wormholes/nodekey 2>/dev/null)
+        if [[ $s -gt 0 ]] && [[ "$key" =~ "nodekey" ]];then
+                echo "Your private key is:"
+                docker exec -it wormholes /usr/bin/cat /wm/.wormholes/wormholes/nodekey
+                echo -ne "\n"
+                docker exec -it wormholes ./wormholes version|grep "Version"|grep -v go
+                break
+        else
+                sleep 5s
+        fi
+done
