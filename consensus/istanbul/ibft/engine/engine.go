55748711904f00718cb9ff64ef5fa81947a6bc2e
mindcarver <626984947@qq.com>
2022-12-22 16:54:16 +0800

    test:add index log

--- consensus/istanbul/ibft/engine/engine.go
+++ consensus/istanbul/ibft/engine/engine.go
@@ -376,8 +376,8 @@ func (e *Engine) Prepare(chain consensus.ChainHeaderReader, header *types.Header
 				// reward to onlineValidtors
 				validatorAddr = append(validatorAddr, v)
 			}
-			for _, v := range commiters {
-				log.Info("print committers", "addr", v.Hex(), "preHeader", preHeader.Number, "no", header.Number)
+			for i, v := range commiters {
+				log.Info("print committers", "len", len(commiters), "i", i, "addr", v.Hex(), "preHeader", preHeader.Number, "no", header.Number)
 			}
 			for _, v := range validatorAddr {
 				log.Info("Prepare: onlineValidator", "addr", v.Hex(), "preHeader", preHeader.Number, "preHash", preHeader.Hash().Hex(), "no", header.Number, "hash", header.Hash().Hex())
