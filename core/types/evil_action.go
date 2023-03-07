00a99127378095b9e69f71c886d4d9792e697540
mindcarver <626984947@qq.com>
2023-03-07 13:30:09 +0800

    perf:Optimize code

--- core/types/evil_action.go
+++ core/types/evil_action.go
@@ -1,6 +1,8 @@
 package types
 
-import "github.com/ethereum/go-ethereum/rlp"
+import (
+	"github.com/ethereum/go-ethereum/rlp"
+)
 
 type EvilAction struct {
 	Handled     bool
@@ -14,6 +16,20 @@ func NewEvilAction(evilHeader *Header) *EvilAction {
 	}
 	evilAction.EvilHeaders = append(evilAction.EvilHeaders, evilHeader)
 	return &evilAction
+}
+
+// @dev Check whether there is an evil header
+func (ea *EvilAction) Exist(uncle *Header) bool {
+	if uncle == nil {
+		return false
+	}
+
+	for _, v := range ea.EvilHeaders {
+		if v.Hash() == uncle.Hash() {
+			return true
+		}
+	}
+	return false
 }
 
 func EncodeEvilAction(ea EvilAction) ([]byte, error) {
