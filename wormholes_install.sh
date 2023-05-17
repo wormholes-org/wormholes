895f118ee29e6a02128f890bf4984e93ab823d61
wormholeslab <dev@wormholes.com>
2023-05-17 10:54:46 +0800

    perf: modify the version to v0.13.2

--- wormholes_install.sh
+++ wormholes_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #check docker cmd
-echo "Script version Number: v0.13.1"
+echo "Script version Number: v0.13.2"
 which docker >/dev/null 2>&1
 if  [ $? -ne 0 ] ; then
         echo "docker not found, please install first!"
