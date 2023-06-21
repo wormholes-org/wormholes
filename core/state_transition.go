a417cf3612539c877ce143d0a3fe8d29ceab35d8
lihaisheng <li.haisheng@wormholeslocal.com>
2023-06-21 13:41:57 +0800

    fix:debug cancel pledge amount judge logic

--- core/state_transition.go
+++ core/state_transition.go
@@ -415,8 +415,10 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 				baseErb, _ := new(big.Int).SetString("1000000000000000000", 10)
 				Erb1000 := big.NewInt(700)
 				Erb1000.Mul(Erb1000, baseErb)
-				if msg.Value().Sign() > 0 && !st.evm.Context.VerifyStakerPledgedBalance(st.state, msg.From(), st.to(), new(big.Int).Add(msg.Value(), Erb1000)) {
-					log.Error("cancel pledge ", ErrInsufficientFundsForTransfer)
+				//if msg.Value().Sign() > 0 && !st.evm.Context.VerifyStakerPledgedBalance(st.state, msg.From(), st.to(), new(big.Int).Add(msg.Value(), Erb1000)) {
+				//	return nil, fmt.Errorf("%w: address %v", ErrInsufficientFundsForTransfer, msg.From().Hex())
+				//}
+				if msg.Value().Sign() > 0 && !st.evm.Context.CanTransfer(st.state, msg.From(), msg.Value()) {
 					return nil, fmt.Errorf("%w: address %v", ErrInsufficientFundsForTransfer, msg.From().Hex())
 				}
 			}
